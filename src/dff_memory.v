baf954ea683aa578721f9f6727bfa8ff7db27cc7
Mike Bell <mdb036@gmail.com>
2024-04-10 18:34:36 +0100

    Instantiate latches

--- src/dff_memory.v
+++ src/dff_memory.v
@@ -4,7 +4,7 @@ module tt_um_urish_256_bits_dff_mem #(
     parameter RAM_BYTES = 32
 ) (
     input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output reg  [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
     input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
     output wire [7:0] uio_out,  // IOs: Bidirectional Output path
     output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
@@ -22,9 +22,26 @@ module tt_um_urish_256_bits_dff_mem #(
 
   reg [7:0] RAM[RAM_BYTES - 1:0];
 
-  always @(*) begin
-      if (wr_en) RAM[addr] = uio_in;
-      uo_out = RAM[addr];
+  genvar i;
+  generate
+  for (i = 0; i < RAM_BYTES; i = i+1) begin
+    wire sel_byte = (addr == i);
+    wire wr_en_this_byte = wr_en && sel_byte;
+    always @(wr_en_this_byte or uio_in)
+        if (wr_en_this_byte)
+            RAM[i] <= uio_in;
   end
+  endgenerate
+
+/*
+    sky130_fd_sc_hd__dlxtp_1 latch[WIDTH-1:0] (
+        .Q(data),
+        .D(data_in_buf),
+        .RESET_B(reset_n),
+        .GATE(empty)
+    );
+*/
+
+  assign uo_out = RAM[addr];
 
 endmodule  // tt_um_urish_256_bits_dff_mem
